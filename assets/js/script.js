be002ee86cad8b6fe64ed33e737f4f0be20bf25d
Baptiste Coulange <bc@cornis.fr>
2013-08-11 22:18:20 +0200

    changement du chermin

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ $(function(){
 	}
 
 	// The URL of your web server (the port is set in app.js)
-	var url = 'http://localhost:8080';
+	var url = 'http://peaceful-bastion-9677.herokuapp.com/';
 
 	var doc = $(document),
 		win = $(window),
