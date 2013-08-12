05d047db1a7bf91c26993db8af21892b10789861
Baptiste Coulange <bc@cornis.fr>
2013-08-12 10:01:15 +0200

    corrections

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,8 +7,8 @@ $(function(){
 	}
 
 	// The URL of your web server (the port is set in app.js)
-//	var url = 'http://peaceful-bastion-9677.herokuapp.com/';
-	var url = 'http://localhost:8080/';
+	var url = 'http://peaceful-bastion-9677.herokuapp.com/';
+	// var url = 'http://localhost:8080/';
 
 	var doc = $(document),
 		win = $(window),
